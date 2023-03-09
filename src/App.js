209d13beddc3b9e2677dd5ab3e7f9799a78dbd01
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-03-09 15:56:52 +0300

    I defined the Provider on the context to make it more manageable and clean

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Button from "./components/Button";
-import ThemeContext from "./context/ThemeContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
   return (
-    <ThemeContext.Provider value="dark">
+    <ThemeProvider>
       <Button />
-    </ThemeContext.Provider>
+    </ThemeProvider>
   );
 }
 
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -2,4 +2,8 @@ import { createContext } from "react";
 
 const ThemeContext = createContext();
 
+export const ThemeProvider = ({ children }) => {
+  return <ThemeContext.Provider value="dark">{children}</ThemeContext.Provider>;
+};
+
 export default ThemeContext;
