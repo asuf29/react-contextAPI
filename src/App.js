e948c7a29e8a236bc57ab8841e5355ba0d3bd083
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-03-16 16:11:18 +0300

    Included in Container App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import Button from "./components/Button";
+import Container from "./components/Container";
 import Header from "./components/Header";
 import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
   return (
     <ThemeProvider>
-      <Header />
-      <hr />
-      <Button />
+      <Container />
     </ThemeProvider>
   );
 }
