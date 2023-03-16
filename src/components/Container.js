bb0d1c90a50bc7cb71a32624e0f36b9dc87aae20
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-03-16 16:06:44 +0300

    Container.js component created

---
+++ src/components/Container.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "./Button";
+import Header from "./Header";
+
+function Container() {
+  return (
+    <div>
+      <Header />
+
+      <hr />
+      <Button />
+    </div>
+  );
+}
+
+export default Container;
