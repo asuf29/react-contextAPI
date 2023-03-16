8cb5dbc8f7e1360f419080c15b9323990b2887b3
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-03-16 16:36:38 +0300

    useContext used

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,11 @@
-import React from "react";
+import { useContext } from "react";
+
 import Button from "./Button";
 import Header from "./Header";
+import ThemeContext from "../context/ThemeContext";
 
 function Container() {
+  const { theme } = useContext(ThemeContext);
   return (
     <div>
       <Header />
